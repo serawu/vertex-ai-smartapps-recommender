0f09e2c1ed1e0b54af0d878b75ad58939de0856c
Seraphina Wu <serawu@deloitte.com>
2022-03-15 23:22:27 -0400

    Updated app.py

--- app.py
+++ app.py
@@ -1,3 +1,12 @@
 from recommender.inference_result import display_inference_result
+from recommender.purchase_history import display_original_purchase_history
+from recommender.bpr_matrix import create_matrix
+from recommender.create_data import create_train_test
+from recommender.bpr import BPR
+
+import pandas as pd
+import numpy as np
+import tensorflow as tf
+import scipy.sparse as sp
 
 display_inference_result()
